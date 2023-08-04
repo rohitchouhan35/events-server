74a4a884e614d290b69f613443b713a7e41fb1fe
rohit <rohitchouhan3512@gmail.com>
2023-08-04 16:26:30 +0530

    geberates link for sign in

--- index.php
+++ index.php
@@ -1,9 +1,26 @@
-<body>
-    <div>
-        <h1>Hello this is Allevent</h1>
-    </div>
-</body>
-</html>
+<?php 
+require_once './vendor/autoload.php';
 
-<!-- client ID: 995843949707-j3mhhl3if5p6gh8docfmkd5klq8oscn4.apps.googleusercontent.com -->
-<!-- client secret: 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 -->
+$clientID = '224762843069-.apps.googleusercontent.com';
+$clientSecret = 'GOCSPX-';
+$redirectUrl = 'http://localhost:3000/';
+
+// creating client request to google
+$client = new Google_Client();
+$client->setClientId($clientID);
+$client->setClientSecret($clientSecret);
+$client->setRedirectUri($redirectUrl);
+$client->addScope('profile');
+$client->addScope('email');
+
+// redirect after login
+if(isset($_GET['code'])) {
+    
+}
+else {
+    echo "<a href='".$client->createAuthUrl()."'>Login with Goggle</a>";
+}
+?>
+<body>
+    <h1>hii</h1>
+</body>
