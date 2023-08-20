4c25df900bd4e1ac8ff52131f886897cf8c90777
rohitchouhan3512 <rohit.chauhan@alphatechsoftsolutions.com>
2023-08-20 14:31:40 +0530

    added list and category filter

--- list-all-events.php
+++ list-all-events.php
@@ -1,44 +1,56 @@
 <?php
-
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
-header('Access-Control-Allow-Methods: GET');
+header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
 
 require './dbcon.php';
 
 $requestMethod = $_SERVER["REQUEST_METHOD"];
 
-if ($requestMethod == "GET") {
-    $eventList = getEventList();
-    echo $eventList; 
+if ($requestMethod == "POST") {
+    $inputData = json_decode(file_get_contents("php://input"), true);
+    $city = isset($inputData['city']) ? $inputData['city'] : null;
+    $category = isset($inputData['category']) ? $inputData['category'] : null;
+    $date = isset($inputData['date']) ? $inputData['date'] : null;
+
+    $eventList = getEventList($city, $category, $date);
+    echo $eventList;
 } else {
     $data = [
         'status' => '405',
         'message' => $requestMethod . ' Method Not Allowed',
     ];
     header("HTTP/1.0 405 Method Not Allowed");
-    echo json_encode($data); 
+    echo json_encode($data);
 }
 
-function getEventList() {
+function getEventList($city, $category, $date) {
     global $conn;
-    $query = "SELECT * FROM events"; 
+
+    $query = "SELECT * FROM events WHERE 1";
+
+    if ($city) {
+        $query .= " AND location = '$city'";
+    }
+    if ($category) {
+        $query .= " AND category = '$category'";
+    }
+    
     $query_run = mysqli_query($conn, $query);
 
     if ($query_run) {
-        if (mysqli_num_rows($query_run) > 0) {
-            $res = mysqli_fetch_all($query_run, MYSQLI_ASSOC);
+        $res = mysqli_fetch_all($query_run, MYSQLI_ASSOC);
 
+        if (!empty($res)) {
             $data = [
                 'status' => '200',
                 'message' => 'Successfully fetched data',
-                'data' => $res 
+                'data' => $res,
             ];
             header("HTTP/1.0 200 OK");
             return json_encode($data);
-        } 
-        else {
+        } else {
             $data = [
                 'status' => '404',
                 'message' => 'No events listed',
@@ -46,8 +58,7 @@ function getEventList() {
             header("HTTP/1.0 404 Not Found");
             return json_encode($data);
         }
-    } 
-    else {
+    } else {
         $data = [
             'status' => '500',
             'message' => 'Internal Server Error',
@@ -56,5 +67,4 @@ function getEventList() {
         return json_encode($data);
     }
 }
-
 ?>
